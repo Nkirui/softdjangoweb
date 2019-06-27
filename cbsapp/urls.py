eca0c700b439abef75a8bd56abac936b1e36f72b
odipojames <odipojames12@gmai.com>
2019-06-27 15:09:43 +0300

    profile creation

---
+++ cbsapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+
+
+urlpatterns = [
+     url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+
+]
--- cbsapp/views.py
+++ cbsapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+def profile(request, username):
+    '''
+    function that returns user profile
+    '''
+    title = "Profile"
+    profile = User.objects.get(username=username)
+    users = User.objects.get(username=username)
+    id = request.user.id
+    form = ProfileForm()
+
+    try :
+        profile_info = Profile.get_by_id(profile.id)
+    except:
+        profile_info = Profile.filter_by_id(profile.id)
+
+    return render(request, 'registration/profile.html', {'title':title,'form':form,'profile_info':profile_info})
