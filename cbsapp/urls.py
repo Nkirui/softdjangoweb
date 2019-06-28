454d5cb6dfc1410f4c17956f659e055886782dff
odipojames <odipojames12@gmai.com>
2019-06-28 15:22:54 +0300

    profile.hml

--- cbsapp/urls.py
+++ cbsapp/urls.py
@@ -7,15 +7,8 @@ from . import views
 
 
 urlpatterns = [
-     # url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
-     # url(r'^profile/new/', views.new_profile, name='newProfile'),
-     # url(r'^Profile/(?P<id>\d+)/update/', views.update_profile, name='updateProfile'),
+
    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
    url(r'^profile/update/', views.update_profile, name='update_profile'),
 
 ]
-
-# ulrpatterns = [
-#         url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
-#         url(r'^profile/update/', views.update_profile, name='update_profile'),
-#     ]
--- cbsapp/views.py
+++ cbsapp/views.py
@@ -25,7 +25,7 @@ def profile(request, username):
     except:
         profile_info = Profile.filter_by_id(profile.id)
 
-    return render(request, 'registration/profile1.html', {'title':title,'form':form,'profile_info':profile_info})
+    return render(request, 'registration/profile.html', {'title':title,'form':form,'profile_info':profile_info})
 
 
 
@@ -57,40 +57,3 @@ def update_profile(request):
         form = ProfileForm()
 
         return render(request, 'registration/updateProfile.html', {'form':form, 'profile_info':profile_info})
-
-
-
-
-
-# @login_required(login_url='/accounts/login/')
-# def new_profile(request,):
-#     current_user = request.user
-#     if request.method =='POST':
-#         form = ProfileForm(request.POST)
-#         if form.is_valid():
-#             profile = form.save(commit=False)
-#             profile.user = current_user
-#             profile.save()
-#             messages.success(request,'profile created successfuly')
-#         return redirect('profile')
-#     else:
-#         form = ProfileForm()
-#         return render(request,'registration/newProfile.html',{"form":form})
-#
-#
-#
-# def update_profile(request,id):
-#     profile = get_object_or_404(Profile,id=id)
-#     if request == 'POST':
-#         form = ProfileForm(request.POST, instance = profile)
-#         try :
-#             if form.is_valid():
-#                 profile = form(commit=False)
-#                 profile.save()
-#                 messages.success('your profile is updated')
-#                 return redirect(home)
-#         except Exception as e:
-#             HttpResponse(request, 'profile not updated')
-#     else :
-#         form = ProfileForm(instance=profile)
-#         return render(request, 'registration/newProfile.html',{'profile':profile,'form':form})
