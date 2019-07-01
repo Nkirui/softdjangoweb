f1ef1025b988c04bfc7b1e056cd176ee5615ab7b
fatahrez <fertahrez@gmail.com>
2019-07-01 10:51:48 +0300

    start styling

--- cbsapp/urls.py
+++ cbsapp/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
     url(r'^profile/update/', views.update_profile, name='update_profile'),
 ]
