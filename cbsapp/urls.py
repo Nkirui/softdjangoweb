3e049a1f79e3136e8a42ed4dd7352f6d245c0fc1
fatahrez <fertahrez@gmail.com>
2019-07-03 10:50:55 +0300

    migrate tp django 2.2 and start changing url to path

--- cbsapp/urls.py
+++ cbsapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .import views
@@ -7,7 +7,7 @@ from .import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
-    url(r'^profile/update/', views.update_profile, name='update_profile'),
+    path(r'^$', views.index, name='index'),
+    path(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    path(r'^profile/update/', views.update_profile, name='update_profile'),
 ]
