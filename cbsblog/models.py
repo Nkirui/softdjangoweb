0183af8815efa2a5ce3d09e33914b96e695e4426
Pancakem <pancakesdeath@protonmail.com>
2019-07-04 19:00:02 +0300

    build each blog can have many categories

--- cbsblog/models.py
+++ cbsblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import permalink
 
 # Create your models here.
 
@@ -8,8 +9,22 @@ class Blog(models.Model):
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     posted_by = models.CharField(max_length=100)
-    category = models.ForeignKey('blog.Category')
+    category = models.ManyToMany('blog.Category')
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_post', None, { 'slug': self.slug })
 
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_category', None, { 'slug': self.slug })
