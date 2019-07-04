e44f187a7ff006683fabc4e665520dd3c0353402
fatahrez <fertahrez@gmail.com>
2019-07-04 22:47:07 +0300

    merge branch with master

--- softsearchcbs/urls.py
+++ softsearchcbs/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
-    re_path('logout/', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    re_path('logout/', auth_views.LogoutView, {'next_page': '/'}, name='logout')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
