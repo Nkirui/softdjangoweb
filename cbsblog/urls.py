9ccafdc7fc4db005b14d6c308dccd6e0b1bac9a1
EUGINELETHAL <ochungeugine@gmail.com>
2019-07-12 11:29:59 +0300

    updated templates

--- cbsblog/urls.py
+++ cbsblog/urls.py
@@ -1,9 +1,7 @@
-
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('blog/', views.PostList.as_view(), name='index'),
-    path('<slug:slug>', views.PostDetail.as_view(), name='post_detail')
+    path('blog/', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ] 
-
