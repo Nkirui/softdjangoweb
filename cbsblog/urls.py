6ac4170b5ee625a95a0a16c86d2a735cf34e6dd6
EUGINELETHAL <ochungeugine@gmail.com>
2019-07-22 18:03:12 +0300

    updated templates

--- cbsblog/urls.py
+++ cbsblog/urls.py
@@ -2,13 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-<<<<<<< HEAD
     path('blog/', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ] 
-=======
-    re_path(r'^blog', views.index, name='index'),
-    re_path(r'^blog/view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post')
-
-]
->>>>>>> 8cccd1e2212f8f0c4d43492dc92044f75578c44a
--- cbsblog/views.py
+++ cbsblog/views.py
@@ -7,13 +7,6 @@ class PostList(generic.ListView):
     template_name = 'index.html'
 
 
-<<<<<<< HEAD
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
-=======
-def view_post(request, slug):
-    return render_to_response('view_post.html', {
-        'post': get_object_or_404(Blog, slug=slug)
-    })
->>>>>>> 8cccd1e2212f8f0c4d43492dc92044f75578c44a
