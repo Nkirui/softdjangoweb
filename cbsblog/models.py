8ea24cffe975694732b91434a16f453d52e1c02d
fatahrez <fertahrez@gmail.com>
2019-07-23 12:00:40 +0300

    fix blog-cbs error

--- cbsblog/models.py
+++ cbsblog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.CharField (max_length=200)
     image = models.ImageField(upload_to='images/')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-created_on']
 
@@ -17,8 +17,6 @@ class Post(models.Model):
         return self.title
 
     def created_on_pretty(self):
-            return self.created_on.strftime('%b %e, %Y')  
+            return self.created_on.strftime('%b %e, %Y')
 
-#it will return you the date in this format – Sep 26, 2018, which looks more user friendly 
-    
-
+#it will return you the date in this format – Sep 26, 2018, which looks more user friendly
--- softsearchcbs/urls.py
+++ softsearchcbs/urls.py
@@ -24,9 +24,8 @@ from cbsapp import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('^', include('django.contrib.auth.urls')),
-    path('', include('cbsapp.urls')),
+    re_path(r'', include('cbsapp.urls')),
     re_path(r'', include('softmain.urls')),
-    re_path(r'', include('cbsblog.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('profile/update',user_views.update_profile, name='update_profile'),
@@ -37,6 +36,7 @@ urlpatterns = [
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    re_path(r'', include('cbsblog.urls')),
 
 
 
