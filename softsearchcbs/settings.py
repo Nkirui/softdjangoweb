ff2a4275447e9a6f0d9575dd6543fecf96b921d7
boyo <boyo@jitu.mshome>
2019-08-27 19:24:51 +0300

    fixes

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sendemail.apps.SendemailConfig',
     'bootstrap3',
     'cbsapp' ,
     'tinymce',
