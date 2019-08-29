0677a3c994afd4e239b5c87280e24f5d08e1a752
boyo <boyo@jitu.mshome>
2019-08-29 11:45:31 +0300

    fixes:

--- softmain/views.py
+++ softmain/views.py
@@ -10,7 +10,7 @@ def home(request):
             sender = email_form.cleaned_data['email']
             subject = email_form.cleaned_data['subject']
             message = email_form.cleaned_data['message']
-            send_mail(subject, message, sender, ['softsearchlimited@gmail.com'])
+            send_mail(subject, message, sender, ['nathankirui5@gmail.com'])
             email_form.save()
             messages.success(request, 'Sent successfully')
     else:
