d6b76e0d91635e2a204dfc6ae6ac377e85f7a9a9
boyo <boyo@jitu.mshome>
2019-08-30 11:31:43 +0300

    fix

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import smtplib
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -171,19 +170,13 @@ SITE_ID=1
 
 
 # sending email
-smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-smtpserver.ehlo()
-smtpserver.starttls()
-smtpserver.ehlo()
-smtpserver.login('nathankirui5@gmail.com', 'N@kirui2015')
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER =  '$EMAIL_HOST_USER'  # os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = '$EMAIL_HOST_PASSWORD' #os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER =  "os.environ.get('EMAIL_HOST_USER')"
+EMAIL_HOST_PASSWORD = "os.environ.get('EMAIL_HOST_PASSWORD')"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 
 # sending sendgrid
 
