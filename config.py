ec3b6653697cc087eb5077893e520810abad09f3
boyo <boyo@jitu.mshome>
2019-08-30 12:07:11 +0300

    fixes

---
+++ config.py
@@ -0,0 +1,4 @@
+EMAIL_HOST_USER =  "nathankirui5@gmail.com" #"os.environ.get('EMAIL_HOST_USER')"
+EMAIL_HOST_PASSWORD = "N@kirui2015" #"os.environ.get('EMAIL_HOST_PASSWORD')"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
