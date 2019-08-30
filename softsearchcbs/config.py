4c400def9b9d94ca7d2c625b728149c7552f050d
boyo <boyo@jitu.mshome>
2019-08-30 12:24:08 +0300

    fixes

--- softsearchcbs/config.py
+++ softsearchcbs/config.py
@@ -1,4 +1,4 @@
-EMAIL_HOST_USER =  "nathankirui5@gmail.com" #"os.environ.get('EMAIL_HOST_USER')"
-EMAIL_HOST_PASSWORD = "N@kirui2015" #"os.environ.get('EMAIL_HOST_PASSWORD')"
+EMAIL_HOST_USER =  "nathankirui5@gmail.com"
+EMAIL_HOST_PASSWORD = "N@kirui2015"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-from .config import * 
 import os
-
+from .config import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LANGUAGE_CODE = 'en-us'
 
 # TIME_ZONE = 'UTC'
-TIME_ZONE = 'Africa/Nairobi'
+
 
 USE_I18N = True
 
@@ -175,8 +174,8 @@ SITE_ID=1
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER =  EMAIL_HOST_USER 
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER =  EMAIL_HOST_USER # "os.environ.get('EMAIL_HOST_USER')"
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD # "os.environ.get('EMAIL_HOST_PASSWORD')"
 EMAIL_PORT = EMAIL_PORT
 EMAIL_USE_SSL = EMAIL_USE_SSL
 
