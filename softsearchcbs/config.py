e49236d5b7faa61a4d4be5a8928710010ea90031
boyo <boyo@jitu.mshome>
2019-08-30 12:52:55 +0300

    fix email

--- softsearchcbs/config.py
+++ softsearchcbs/config.py
@@ -1,4 +1,4 @@
-EMAIL_HOST_USER =  "nathankirui5@gmail.com"
-EMAIL_HOST_PASSWORD = "N@kirui2015"
+EMAIL_HOST_USER =  "softsearchlimited@gmail.com"
+EMAIL_HOST_PASSWORD = "Bashir_@12"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -174,22 +174,8 @@ SITE_ID=1
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER =  EMAIL_HOST_USER # "os.environ.get('EMAIL_HOST_USER')"
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD # "os.environ.get('EMAIL_HOST_PASSWORD')"
+EMAIL_HOST_USER =  EMAIL_HOST_USER 
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD 
 EMAIL_PORT = EMAIL_PORT
 EMAIL_USE_SSL = EMAIL_USE_SSL
 
-# sending sendgrid
-
-#API_CODE="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"
-
-#SENDGRID_API_KEY = API_CODE  # os.getenv('SENDGRID_API_KEY')
-
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# #EMAIL_HOST_USER = 'apikey'
-# #EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-# EMAIL_HOST_USER = 'nathankirui5@gmail.com'
-# EMAIL_HOST_PASSWORD = 'monkey567'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'nathankirui5@gmail.com'
