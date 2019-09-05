70e8729aec4f7986119e4973ee004b7601538dea
boyo <boyo@jitu.mshome>
2019-09-05 11:39:01 +0300

    remove postgres

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -95,7 +95,14 @@ WSGI_APPLICATION = 'softsearchcbs.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if 'RDS_DB_NAME' in os.environ:
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+
+
+"""if 'RDS_DB_NAME' in os.environ:
 
     DATABASES = {
         'default': {
@@ -115,7 +122,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+"""
 
 
 # Password validation
