39f602647eecd0d356c182b1683b53fb31b5e1cf
boyo <boyo@jitu.mshome>
2019-09-08 00:28:58 +0300

    db fixes

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -94,16 +94,17 @@ WSGI_APPLICATION = 'softsearchcbs.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # RDS variables come automatically from AWS RDS
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 
 
 
