ef8d1b87127e362b55fe77c5584d18604c11c71a
boyo <boyo@jitu.mshome>
2019-09-10 16:20:17 +0300

    travisall

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -96,16 +96,11 @@ WSGI_APPLICATION = 'softsearchcbs.wsgi.application'
 # RDS variables come automatically from AWS RDS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-
-
-
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
