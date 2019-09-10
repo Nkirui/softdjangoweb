ff4a71de7b86c8159131c86415efcbb023759356
boyo <boyo@jitu.mshome>
2019-09-10 15:52:22 +0300

    travisall

--- softsearchcbs/settings.py
+++ softsearchcbs/settings.py
@@ -94,6 +94,8 @@ WSGI_APPLICATION = 'softsearchcbs.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # RDS variables come automatically from AWS RDS
+
+if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -102,6 +104,14 @@ WSGI_APPLICATION = 'softsearchcbs.wsgi.application'
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
